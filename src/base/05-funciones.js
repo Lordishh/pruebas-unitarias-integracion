e7bff3c8f044864680e46719645ba9666b16cea5
Enuel <enuel.monzon@gmail.com>
2022-01-03 17:57:37 -0300

    test toEqual

--- src/base/05-funciones.js
+++ src/base/05-funciones.js
@@ -8,15 +8,10 @@ const saludar2 = (nombre) => {
   return `Hola, soy ${nombre}`;
 };
 
-// Si es una arrow de una sola sentencia, podríamos simplificarlo de la siguiente manera:
 const saludar3 = (nombre) => `Hola, soy ${nombre}`;
 
-console.log(saludar("Goku"));
-console.log(saludar2("Goku"));
-console.log(saludar3("Goku"));
-
 //Paréntesis para objetos en funciones flecha
-const getUser = () => ({
+export const getUser = () => ({
   uid: "ABC123",
   username: "El_Papi1502",
 });
@@ -26,7 +21,7 @@ console.log(user);
 
 //Tarea
 
-const getUsuarioActivo = (nombre) => ({
+export const getUsuarioActivo = (nombre) => ({
   uid: "ABC567",
   username: nombre,
 });
