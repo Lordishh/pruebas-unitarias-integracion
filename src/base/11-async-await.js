16ea98e9db691f78d1efd2230b76e6f6a330771d
Enuel <enuel.monzon@gmail.com>
2022-01-04 15:41:29 -0300

    Pruebas con async-await

--- src/base/11-async-await.js
+++ src/base/11-async-await.js
@@ -1,31 +1,17 @@
-const getImagen = async () => {
+export const getImagen = async () => {
   try {
-    const apiKey = "GsZ5KGD73v1qKAoExOuyJpPqlprtWsoc";
+    const apiKey = "BNbKvt5wAkkVU83Bz2vFjWpVVvuQPCoP";
     const resp = await fetch(
       `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
     );
     const { data } = await resp.json();
-    console.log(data);
+    // console.log(data);
     const { url } = data.images.original;
-    const img = document.createElement("img");
-    img.src = url;
 
-    document.body.append(img);
+    return url;
   } catch (error) {
-    console.log(error);
+    //manejo del error
+    // console.log(error);
+    return "No existe";
   }
 };
-
-getImagen();
-
-// peticion
-//   .then((res) => res.json())
-//   .then(({ data }) => {
-//     const { url } = data.images.original;
-
-//     const img = document.createElement("img");
-//     img.src = url;
-
-//     document.body.append(img);
-//   })
-//   .catch(console.warn);
